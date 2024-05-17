0347f057210d27d9f5b0d4ca00ecb30ae63e9007
Gerardo Lanza <glanza007@gmail.com>
2024-05-17 16:54:55 -0600

    Creacion de la arquitectura del proyecto

---
+++ InventarioPro/InventarioPro.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InventarioPro.Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
